475376058880e01aeec7a5dbdfb6b8a03b1c2fb7
Josh Diaz <josh@iserveproducts.com>
2019-05-05 22:38:20 -0600

    Seeds update

--- app/controllers/api/v1/breeds_controller.rb
+++ app/controllers/api/v1/breeds_controller.rb
@@ -5,6 +5,23 @@ module Api
         breeds = Breed.all
         render json: { data: breeds }, status: :ok
       end
+
+      def create
+        breed = breed.new(breed_params)
+        if breed.save
+          render json: { data: breed }, status: :ok
+        else
+          render json: { data: breed.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def breed_params
+        params.permit(
+          :name,
+        )
+      end
     end
   end
 end
--- app/controllers/api/v1/eye_colors_controller.rb
+++ app/controllers/api/v1/eye_colors_controller.rb
@@ -5,6 +5,23 @@ module Api
         eye_colors = EyeColor.all
         render json: { data: eye_colors }, status: :ok
       end
+
+      def create
+        eye = eye.new(eye_params)
+        if eye.save
+          render json: { data: eye }, status: :ok
+        else
+          render json: { data: eye.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def eye_params
+        params.permit(
+          :name,
+        )
+      end
     end
   end
 end
